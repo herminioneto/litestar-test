bce830f81484c7e982b28e64110c17110a211f44
herminioneto <herminio_neto@outlook.com>
2024-12-30 16:33:06 -0300

    configured sqlalchemy with sqlite

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from litestar import Litestar, get
+from advanced_alchemy.extensions.litestar.plugins.init.config.asyncio import autocommit_before_send_handler
+from litestar import Litestar
+from litestar.contrib.sqlalchemy.plugins import SQLAlchemyAsyncConfig, SQLAlchemyPlugin
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-@get('/')
-async def main() -> str:
-    return 'Hello World!'
+class Base(DeclarativeBase):
+    pass
 
 
-app = Litestar([main])
+class Todo(Base):
+    __tablename__ = 'todo_items'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    task: Mapped[str]
+    user_id: Mapped[int]
+
+
+db_config = SQLAlchemyAsyncConfig(
+    connection_string='sqlite+aiosqlite:///db.sqlite',
+    metadata=Base.metadata,
+    create_all=True,
+    before_send_handler=autocommit_before_send_handler,
+)
+
+app = Litestar([], plugins=[SQLAlchemyPlugin(db_config)])
